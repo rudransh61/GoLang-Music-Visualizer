42dcd2b6e811b09f672e97c355ace69eaae0ccb8
Rudransh Bhardwaj <aarti19830@gmail.com>
2024-02-29 16:20:28 +0530

    done with image'

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
 	"github.com/faiface/beep/wav"
-	"os"
-	"time"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
 )
 
 const (
-	sampleRate   beep.SampleRate = 44100
+	sampleRate beep.SampleRate = 44100
+	size       int             = 100
 )
 
-const  size  int = 100 
 func main() {
 	// Load the audio file
 	fmt.Print("Enter your file name (.wav): ")
@@ -71,22 +73,7 @@ func calculateIntensity(sample [2]float64) float64 {
 }
 
 func printCoordinates(coordinates [][]float64) {
-	// Print the coordinates
-	
-	// var space = [100][2]float64{};
-	
-	// for i, coord := range coordinates {
-		// fmt.Printf("[%d] Time: %.2f, Intensity: %.2f\n", i, coord[0]/float64(sampleRate), coord[1])
-		// space.append(coord[0], coord[1])
-		// fmt.Printf("{},{}",coord[0],coord[1])
-
-	// }
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Printf("*************************")
-	// }
-
 	// Create a 2D array 'space' representing the graph
-
 	var space [size][size]rune // Assuming a fixed size of 100x100
 
 	for i := range space {
@@ -98,25 +85,66 @@ func printCoordinates(coordinates [][]float64) {
 	// Plot each point from coordinates onto the graph
 	for _, coord := range coordinates {
 		x, y := int(coord[0]/1000), int(coord[1]*10)
-		// fmt.Println(x,y)
 		if x >= 0 && x < size && y >= 0 && y < size {
 			space[y][x] = '*'
 		}
 	}
-	//Print space upside down
+	// Print space upside down
 	for i := len(space) - 1; i >= 0; i-- {
 		for j := range space[i] {
-			if(i<3){
+			if i < 3 {
 				fmt.Printf("\x1b[34m%c\x1b[0m", space[i][j])
 			}
-			if(i>=3 && i<=4){
+			if i >= 3 && i <= 4 {
 				fmt.Printf("\x1b[32m%c\x1b[0m", space[i][j])
 			}
-			if(i>4){
+			if i > 4 {
 				fmt.Printf("\x1b[31m%c\x1b[0m", space[i][j])
 			}
-			// fmt.Printf("\x1b[31m%c\x1b[0m", space[i][j])
 		}
 		fmt.Println()
 	}
+
+	// Plot using Gonum
+	plotGraph(space)
+}
+func plotGraph(space [size][size]rune) {
+	// Create a plot
+	p := plot.New()
+	// if err != nil {
+	// 	fmt.Println("Error creating plot:", err)
+	// 	return
+	// }
+
+	// Create a scatter plotter
+	scatter, err := plotter.NewScatter(getXYs(space))
+	if err != nil {
+		fmt.Println("Error creating scatter plotter:", err)
+		return
+	}
+
+	// Add the scatter plotter to the plot
+	p.Add(scatter)
+
+	// Save the plot to a file
+	if err := p.Save(4, 4, "output.png"); err != nil {
+		fmt.Println("Error saving plot:", err)
+		return
+	}
+
+	fmt.Println("Plot saved as output.png. Open the file with an image viewer.")
+}
+
+func getXYs(space [size][size]rune) plotter.XYs {
+	var XYs plotter.XYs
+
+	for i, row := range space {
+		for j, value := range row {
+			if value == '*' {
+				XYs = append(XYs, plotter.XY{X: float64(j*10), Y: float64((size - 1 - i)*10)})
+			}
+		}
+	}
+
+	return XYs
 }
